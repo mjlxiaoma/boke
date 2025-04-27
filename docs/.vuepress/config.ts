4b7ecf51766608be00bd5011de8ef0b9e8d6dfbf
小马同学 <89441679+m2075498242@users.noreply.github.com>
2025-04-27 14:12:37 +0800

    fix:url

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -20,6 +20,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     }
   },
   dest: './dist',   // 设置输出目录
+  base: '/boke/',
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
