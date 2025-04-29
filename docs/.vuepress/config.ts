511e0c3492901ee2233d613c58daa9696784f6a7
小马同学 <89441679+m2075498242@users.noreply.github.com>
2025-04-29 13:25:01 +0800

    fix:github地址修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -149,7 +149,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/cometong',
+          link: 'https://github.com/mjlxiaoma',
         }
       ],
     },
@@ -158,14 +158,14 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2019, // 博客创建年份
       copyrightInfo:
-        'comelong | <a href="https://github.com/comelong" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+        'comelong | <a href="https://github.com/mjlxiaoma" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
     extendFrontmatter: {
       author: {
         name: 'comelong',
-        link: 'https://github.com/comelong'
+        link: 'https://github.com/mjlxiaoma'
       }
     },
 
